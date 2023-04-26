18ecd1c6f979799ba85087a3aedc1b8b52949488
chernnovaa <katac0747@gmail.com>
2023-04-26 16:10:09 +0200

    Added menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu__list--active');
+});
